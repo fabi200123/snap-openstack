d847dd45d36f73713a4b90e26bfdeb150a08d3a8
Fabian Fulga <ffulga@cloudbasesolutions.com>
2025-06-11 15:46:40 +0300

    Minor fix

--- sunbeam-python/sunbeam/features/tls/ca.py
+++ sunbeam-python/sunbeam/features/tls/ca.py
@@ -191,16 +191,6 @@ class ConfigureCAStep(BaseStep):
             return Result(ResultType.FAILED, str(e))
 
         LOG.debug(f"Process certs: {self.process_certs}")
-        certs = re.findall(
-            r"(-----BEGIN CERTIFICATE-----.+?-----END CERTIFICATE-----)",
-            self.ca_chain,
-            flags=re.DOTALL,
-        )
-        if not certs:
-            LOG.error("No certificates found in ca_chain")
-            return Result(ResultType.FAILED, "Invalid CA chain")
-
-        first_cert = certs[0]
         for subject, request in self.process_certs.items():
             csr = request.get("csr")
             csr = encode_base64_as_string(csr)
@@ -209,7 +199,7 @@ class ConfigureCAStep(BaseStep):
 
             action_params = {
                 "relation-id": request.get("relation_id"),
-                "certificate": first_cert,
+                "certificate": request.get("certificate"),
                 "ca-chain": self.ca_chain,
                 "ca-certificate": self.ca_cert,
                 "certificate-signing-request": str(csr),
@@ -222,7 +212,7 @@ class ConfigureCAStep(BaseStep):
                     self.jhelper.run_action(unit, self.model, action_cmd, action_params)
                 )
             except ActionFailedException as e:
-                LOG.debug(f"Running action {action_cmd} on {unit} failed")
+                LOG.debug(f"Running action {action_cmd} on {unit} with params {action_params} failed")
                 return Result(ResultType.FAILED, str(e))
 
             LOG.debug(f"Result from action {action_cmd}: {action_result}")
