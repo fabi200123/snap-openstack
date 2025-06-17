e5f3a81f60bab524c2eda87746fa9580652d7867
Fabian Fulga <ffulga@cloudbasesolutions.com>
2025-06-17 23:42:31 +0300

    Disable TLS Ca for testing purposes

--- sunbeam-python/sunbeam/features/tls/feature.py
+++ sunbeam-python/sunbeam/features/tls/feature.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2024 - Canonical Ltd
 # SPDX-License-Identifier: Apache-2.0
-from .ca import CaTlsFeature  # noqa: F401
+# from .ca import CaTlsFeature  # noqa: F401
 from .vault import VaultTlsFeature  # noqa: F401
