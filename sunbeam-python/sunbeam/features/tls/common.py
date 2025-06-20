f18a747dc9f3f5cf895505209835c5a5d7746124
Fabian Fulga <ffulga@cloudbasesolutions.com>
2025-06-20 16:56:25 +0300

    Add Sunbeam TLS Vault Feature

--- sunbeam-python/sunbeam/features/tls/common.py
+++ sunbeam-python/sunbeam/features/tls/common.py
@@ -10,6 +10,7 @@ from rich.console import Console
 from rich.status import Status
 
 from sunbeam.clusterd.service import ConfigItemNotFoundException
+from sunbeam.core import questions
 from sunbeam.core.common import (
     BaseStep,
     Result,
@@ -25,7 +26,11 @@ from sunbeam.core.juju import (
     LeaderNotFoundException,
     run_sync,
 )
-from sunbeam.core.manifest import FeatureConfig
+from sunbeam.core.manifest import (
+    CharmManifest,
+    FeatureConfig,
+    SoftwareConfig,
+)
 from sunbeam.core.openstack import OPENSTACK_MODEL
 from sunbeam.features.interface.v1.base import BaseFeatureGroup
 from sunbeam.features.interface.v1.openstack import (
@@ -80,6 +85,31 @@ class TlsFeature(OpenStackControlPlaneFeature):
     @click.group()
     def disable_tls(self) -> None:
         """Disable TLS group."""
+
+    @click.group()
+    def tls_group(self):
+        """Manage TLS."""
+
+    def default_software_overrides(self) -> SoftwareConfig:
+        """Feature software configuration."""
+        return SoftwareConfig(
+            charms={"manual-tls-certificates": CharmManifest(
+                channel="latest/stable",)}
+        )
+
+    def manifest_attributes_tfvar_map(self) -> dict:
+        """Manifest attributes terraformvars map."""
+        return {
+            self.tfplan: {
+                "charms": {
+                    "manual-tls-certificates": {
+                        "channel": "manual-tls-certificates-channel",
+                        "revision": "manual-tls-certificates-revision",
+                        "config": "manual-tls-certificates-config",
+                    }
+                }
+            }
+        }
 
     def provider_config(self, deployment: Deployment) -> dict:
         """Return stored provider configuration."""
@@ -332,3 +362,26 @@ class RemoveCACertsFromKeystoneStep(BaseStep):
             )
 
         return Result(ResultType.COMPLETED)
+
+
+def certificate_questions(unit: str, subject: str):
+    return {
+        "certificate": questions.PromptQuestion(
+            f"Base64 encoded Certificate for {unit} CSR Unique ID: {subject}",
+        ),
+    }
+
+
+def get_outstanding_certificate_requests(
+    app: str, model: str, jhelper: JujuHelper
+) -> dict:
+    """Get outstanding certificate requests from manual-tls-certificate
+    operator.
+
+    Returns the result from the action get-outstanding-certificate-requests
+    Raises LeaderNotFoundException, ActionFailedException.
+    """
+    action_cmd = "get-outstanding-certificate-requests"
+    unit = run_sync(jhelper.get_leader_unit(app, model))
+    action_result = run_sync(jhelper.run_action(unit, model, action_cmd))
+    return action_result
