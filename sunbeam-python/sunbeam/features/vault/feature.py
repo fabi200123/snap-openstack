584c5fb82253feda4039ab627686cbe85a207a7c
Fabian Fulga <ffulga@cloudbasesolutions.com>
2025-07-17 03:23:49 +0300

    Block disable of Vault if TLS Vault is enabled

--- sunbeam-python/sunbeam/features/vault/feature.py
+++ sunbeam-python/sunbeam/features/vault/feature.py
@@ -656,8 +656,18 @@ class VaultFeature(OpenStackControlPlaneFeature):
     @pass_method_obj
     def disable_cmd(self, deployment: Deployment, show_hints: bool) -> None:
         """Disable Vault."""
-        self.disable_feature(deployment, show_hints)
         client = deployment.get_client()
+        try:
+            tls_cfg = read_config(client, "TlsProvider")
+        except ConfigItemNotFoundException:
+            tls_cfg = {}
+        if tls_cfg.get("provider") == "tls.vault":
+            raise click.ClickException(
+                "Vault is currently acting as your TLS provider. "
+                "Please run `sunbeam disable tls vault` first."
+            )
+
+        self.disable_feature(deployment, show_hints)
         update_config(client, VAULT_DEV_MODE_KEY, {})
 
     @click.command()
