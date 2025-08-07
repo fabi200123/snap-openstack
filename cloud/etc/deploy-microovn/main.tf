df2c942ffabe58453d1e641d7b3d0b07a98fa611
Fabian Fulga <ffulga@cloudbasesolutions.com>
2025-08-07 14:42:40 +0300

    Remove snap-channel

--- cloud/etc/deploy-microovn/main.tf
+++ cloud/etc/deploy-microovn/main.tf
@@ -27,9 +27,6 @@ resource "juju_application" "microovn" {
     base     = "ubuntu@24.04"
   }
 
-  config = merge({
-    snap-channel = var.microovn_channel
-  }, var.charm_microovn_config)
   endpoint_bindings = var.endpoint_bindings
 }
 
