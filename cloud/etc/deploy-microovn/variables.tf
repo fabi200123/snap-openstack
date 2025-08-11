fb8c47c0665ed2a95f9d00f1b95447dbb8a6b165
Fabian Fulga <ffulga@cloudbasesolutions.com>
2025-08-12 00:31:27 +0300

    Small fix

--- cloud/etc/deploy-microovn/variables.tf
+++ cloud/etc/deploy-microovn/variables.tf
@@ -1,4 +1,6 @@
-# variables.tf additions (or ensure they exist)
+# SPDX-FileCopyrightText: 2023 - Canonical Ltd
+# SPDX-License-Identifier: Apache-2.0
+
 variable "charm_microovn_channel" {
   type    = string
   default = "latest/edge"
@@ -8,6 +10,28 @@ variable "microovn_snap_channel" {
   description = "MicroOVN snap channel to install via the charm"
   type        = string
   default     = "latest/edge"
+}
+
+variable "charm_microovn_config" {
+  description = "Operator config for microovn deployment"
+  type        = map(string)
+  default     = {}
+}
+
+variable "microovn_channel" {
+  description = "K8S channel to deploy, not the operator channel"
+  default     = "latest/edge"
+}
+
+variable "machine_ids" {
+  description = "List of machine ids to include"
+  type        = list(string)
+  default     = []
+}
+
+variable "machine_model" {
+  description = "Model to deploy to"
+  type        = string
 }
 
 variable "endpoint_bindings" {
@@ -23,15 +47,57 @@ variable "endpoint_bindings" {
   ]
 }
 
-variable "ca-offer-url" {
-  type    = string
-  default = null
+# Mandatory relation, no defaults
+variable "rabbitmq-offer-url" {
+  description = "Offer URL for openstack rabbitmq"
+  type        = string
 }
 
+# Mandatory relation, no defaults
+variable "keystone-offer-url" {
+  description = "Offer URL for openstack keystone identity-credentials relation"
+  type        = string
+}
+
+variable "cert-distributor-offer-url" {
+  description = "Offer URL for openstack keystone certificate-transfer relation"
+  type        = string
+  default     = null
+}
+
+variable "ca_offer_url" {
+  description = "Offer URL for Certificates"
+  type        = string
+  default     = "sunbeam/openstack.certificate-authority"
+}
+
+# Mandatory relation, no defaults
 variable "ovn-relay-offer-url" {
-  type    = string
-  default = null
+  description = "Offer URL for ovn relay service"
+  type        = string
+  default     = "sunbeam/openstack.ovn-relay"
 }
 
-variable "machine_model" { type = string }
-variable "machine_ids"   { type = list(string) default = [] }
+variable "ceilometer-offer-url" {
+  description = "Offer URL for openstack ceilometer"
+  type        = string
+  default     = null
+}
+
+variable "cinder-volume-ceph-application-name" {
+  description = "Name for cinder-volume-ceph application"
+  type        = string
+  default     = null
+}
+
+# Mandatory relation, no defaults
+variable "nova-offer-url" {
+  description = "Offer URL for openstack nova"
+  type        = string
+}
+
+variable "masakari-offer-url" {
+  description = "Offer URL for openstack masakari"
+  type        = string
+  default     = null
+}
